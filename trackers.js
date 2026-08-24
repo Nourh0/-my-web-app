677f2606d0f0e7705996849e84dbbd73e7b5c9cd
Nourh0 <nooon-25-25@hotmail.com>
2026-08-24 23:01:00 +0300

    Update tracker list for 2024 with new entries

--- trackers.js
+++ trackers.js
@@ -1,19 +1,19 @@
-// قائمة بأسرع التراكرز عالمياً لتسريع جلب الموزعين
+// قائمة التراكرز المحدثة 2024 - فائقة السرعة
 const trackers = [
     "udp://tracker.opentrackr.org:1337/announce",
-    "udp://9.rarbg.com:2810/announce",
-    "udp://tracker.openbittorrent.com:6969/announce",
-    "udp://exodus.desync.com:6969",
     "udp://open.stealth.si:80/announce",
     "udp://tracker.torrent.eu.org:451/announce",
     "udp://tracker.moeking.me:6969/announce",
-    "udp://p4p.arenabg.com:1337/announce",
+    "udp://exodus.desync.com:6969/announce",
+    "udp://tracker.bit-cloud.me:6969/announce",
+    "udp://opentracker.i2p.rocks:6969/announce",
     "udp://tracker.tiny-vps.com:6969/announce",
+    "udp://p4p.arenabg.com:1337/announce",
+    "udp://9.rarbg.me:2970/announce",
     "udp://tracker.cyberia.is:6969/announce",
     "http://tracker.openbittorrent.com:80/announce",
-    "udp://opentracker.i2p.rocks:6969/announce",
-    "udp://46.148.18.250:2710/announce",
-    "udp://explodie.org:6969/announce"
+    "udp://fe.omda.top:6969/announce",
+    "udp://tracker.pomf.se:80/announce"
 ];
 
 module.exports = trackers;
